b449d7a9b721d43983e6e2d045c03ebe970cf818
unknown <hwangninaa@gmail.com>
2020-12-03 14:08:31 +0900

    [ADD] 고객사 모델

--- company/models.py
+++ company/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Group(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta():
+        db_table = "groups"
+
+
+class Company(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    name  = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta():
+        db_table = "companies"
+
+
+class Representative(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    phone_num = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta():
+        db_table = "representatives"
